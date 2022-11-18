3a9c57f16a693cb0e62b2bfa8a2c98ca3f00cbf1
scmiguel <miguelsouzacintra@gmail.com>
2022-11-18 15:17:53 +0000

    18-11

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
     // Precisa terminar com /
-    baseURL: 'https://agoravai-miguel.onrender.com/'  
+    baseURL: 'https://agoravai-miguel.onrender.com/',
+    timeout: 5000,
+    headers: {'x-access-token': window.sessionStorange.getItem('app-data')}
 })
 
 export default api
